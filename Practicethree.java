54f483e03131b680912b7ecc18670a32b72fbec6
FLWRboy1 <frankmullahy@gmail.com>
2026-03-02 21:02:23 -0500

    a

--- Practicethree.java
+++ Practicethree.java
@@ -4,7 +4,13 @@ public class Practicethree {
 
         String [][] arr = { {"Jack","Jill","Jane","Joey"} , {"Maura","Molly","Mason","Maureen"} , {"Emma","Eileen","Elizabeth","Emma"} };
 
-        System.out.println( shortenMe(arr, arr.length, arr[0].length) );
+        String [][] shortened = shortenMe(arr, arr.length, arr[0].length);
+        printArray(shortened);
+
+        System.out.println( countduplicates(arr) );
+
+        String [][] shifted = shiftMe(arr);
+        printArray(shifted);
 
     }
 
@@ -18,13 +24,11 @@ public class Practicethree {
 
                 if(array[row][col].length() <= 3){
 
-                    String x = array[row][col];
-                    array[row][col] = x;
+                    shortArray[row][col] = array[row][col];
 
                 }else{
     
-                    String x = array[row][col].substring(0,4);
-                    array[row][col] = x;
+                    shortArray[row][col] = array[row][col].substring(0,3);
 
                 }
 
@@ -33,6 +37,101 @@ public class Practicethree {
         }
 
         return shortArray;
+
+    }
+
+    public static int countduplicates( String [][] array ){
+
+        int maxRow = 0;
+        int maxDuplicates = 0;
+
+        for(int row = 0; row < array.length; row++){
+
+            int duplicates = 0;
+
+            for(int i = 0; i < array[row].length; i++){
+
+                for(int j = i + 1; j < array[row].length; j++){
+
+                    if(array[row][i].equals(array[row][j])){
+
+                        duplicates++;
+
+                    }
+
+                }
+
+            }
+
+            if(duplicates > maxDuplicates){
+
+                maxDuplicates = duplicates;
+                maxRow = row;
+
+            }
+
+        }
+
+        return maxRow;
+
+    }
+
+    public static String [][] shiftMe( String [][] array ){
+
+        int rows = array.length;
+        int cols = array[0].length;
+
+        String [][] shifted = new String[rows][cols];
+
+        shifted[0][0] = array[rows - 1][cols - 1];
+
+        int r = 0;
+        int c = 1;
+
+        for(int row = 0; row < rows; row++){
+
+            for(int col = 0; col < cols; col++){
+
+                if(row == rows - 1 && col == cols - 1){
+
+                    continue;
+
+                }
+
+                shifted[r][c] = array[row][col];
+
+                c++;
+
+                if(c == cols){
+
+                    c = 0;
+                    r++;
+
+                }
+
+            }
+
+        }
+
+        return shifted;
+
+    }
+
+    public static void printArray( String [][] array ){
+
+        for(int row = 0; row < array.length; row++){
+
+            for(int col = 0; col < array[0].length; col++){
+
+                System.out.print(array[row][col] + "\t");
+
+            }
+
+            System.out.println();
+
+        }
+
+        System.out.println();
 
     }
 
